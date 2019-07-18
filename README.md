c28854c4a28e60c2088b1b2673d6a26c06aaaed2
David Sickmiller <david@asknice.ly>
2019-07-19 09:31:52 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+AskNicely PHP backend skill test
+==========================
+
+
+### Application
+The TODO App allows a user to add reminders of thing he needs to do. Here are the requirement for the app.
+* Users can add, delete and see their todos.
+* All the todos are private, users can't see other user's todos.
+* Users must be logged in order to add/delete/see their todos.
+
+Credentials:
+* username: **user1**
+* password: **user1**
+
+#### Homepage:
+![Homepage](/web/img/homepage.png?raw=true "Homepage")
+
+#### Login page:
+![Login page](/web/img/login-page.png?raw=true "Login page")
+
+#### Todos:
+![Todos](/web/img/todos.png?raw=true "Todos")
+
+### Requirements
+* php 5.3+
+* mysql
+* A github account
+
+### Installation
+**/!\ You need to fork this repository. See [How to submit your work?](#how-to-submit-your-work)**
+```sh
+php composer.phar install
+cp config/config.yml.dist config/config.yml
+mysql -u root <database> < resources/database.sql
+mysql -u root <database> < resources/fixtures.sql
+php -S localhost:1337 -t web/ web/index.php
+```
+You can change the database connection from the file `config/config.yml`.
+
+### Instructions
+
+You will be asked to improve the code of this app with the following tasks.
+
+You can complete the tasks in any order.
+
+### What we are looking for?
+1. Separate your <b>commits by task</b> and use the following format for your commit messages: TASK-{task number}: {meaningful message}
+2. We care about UI/UX, any attention to detail in the UI will be noticed. Please dont hack in UI changes. 
+3. Simple clear code comments are helpful.   
+
+### Task
+Using JQuery, Vue.js, or React render the todo list dynamically and allow the delete + completed buttons to work dynamically via an Ajax<br>
+ (You do not need to make pagination to work). We care about the user experience here, this might be animation?  
+ 
+    
+
+Extra tasks:
+- Fix any bugs you may find.
+- Fix any security issues you may find.
+- Adding a few unit tests to show us that you undersatnd how they work is a bonus. 
+
+### Documentation
+This app use [Silex](http://silex.sensiolabs.org/), a  micro-framework based on the Symfony2 Components.
+Documentation can be found here: http://silex.sensiolabs.org/documentation
+
+
+### How to submit your work?
+
+1. ##### First you need to fork this repository.
+![Forking a repo](/web/img/fork.png?raw=true "Forking a repo")
+
+2. ##### Then clone your fork locally.
+![Cloning a repo](/web/img/clone.png?raw=true "Cloning a repo")
+
+3. ##### Install the app locally. See the [Installation Guide] (#Installation).
+
+4. ##### Once you've completed your work, you can submit a pull-request to the remote repository.
+![ a Pull Request](/web/img/pull-request.png?raw=true "Creating a Pull Request")
+
+5. ##### Review your changes and validate.
+![Validating a Pull Request](/web/img/pull-request-review.png?raw=true "Validating a Pull Request")
+
+
+
+And you're done!
+
+
+More documentation on Github:
+* https://help.github.com/articles/fork-a-repo/
+* https://help.github.com/articles/using-pull-requests/
+
---
+++ src/controllers.php
@@ -0,0 +1,124 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    $twig->addGlobal('user', $app['session']->get('user'));
+
+    return $twig;
+}));
+
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html', [
+        'readme' => file_get_contents('README.md'),
+    ]);
+});
+
+
+$app->match('/login', function (Request $request) use ($app) {
+    $username = $request->get('username');
+    $password = $request->get('password');
+
+    if ($username) {
+        $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
+        $user = $app['db']->fetchAssoc($sql);
+
+        if ($user){
+            $app['session']->set('user', $user);
+            return $app->redirect('/todo');
+        }
+    }
+
+    return $app['twig']->render('login.html', array());
+});
+
+
+$app->get('/logout', function () use ($app) {
+    $app['session']->set('user', null);
+    return $app->redirect('/');
+});
+
+
+$app->get('/todo/{id}', function ($id, Request $request) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    $contentType = $request->headers->get('Content-Type');
+
+    if ($id){
+        $sql = "SELECT * FROM todos WHERE id = '$id'";
+        $todo = $app['db']->fetchAssoc($sql);
+
+        if (strpos($contentType, 'application/json') === false) {
+            return $app['twig']->render('todo.html', [
+                'todo' => $todo,
+            ]);
+        } else {
+            return json_encode($todo);
+        }
+
+    } else {
+        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
+        $todos = $app['db']->fetchAll($sql);
+
+        if (strpos($contentType, 'application/json') === false) {
+            return $app['twig']->render('todos.html', [
+                'todos' => $todos,
+            ]);
+        } else {
+            return json_encode($todos);
+        }
+    }
+})
+->value('id', null);
+
+
+$app->post('/todo/add', function (Request $request) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    $user_id = $user['id'];
+    $description = $request->get('description');
+    $contentType = $request->headers->get('Content-Type');
+
+    $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
+    $app['db']->executeUpdate($sql);
+
+    if (strpos($contentType, 'application/json') === false) {
+        return $app->redirect('/todo');
+    } else {
+        return json_encode(array('success' => true));
+    }
+});
+
+
+$app->match('/todo/delete/{id}', function (Request $request, $id) use ($app) {
+
+    $sql = "DELETE FROM todos WHERE id = '$id'";
+    $app['db']->executeUpdate($sql);
+
+    $contentType = $request->headers->get('Content-Type');
+    if (strpos($contentType, 'application/json') === false) {
+        return $app->redirect('/todo');
+    } else {
+        return json_encode(array('success' => true));
+    }
+});
+
+
+$app->match('/todo/complete/{id}', function (Request $request, $id) use ($app) {
+
+    $sql = "UPDATE todos SET completed = 1 WHERE id = '$id'";
+    $app['db']->executeUpdate($sql);
+
+    $contentType = $request->headers->get('Content-Type');
+    if (strpos($contentType, 'application/json') === false) {
+        return $app->redirect('/todo');
+    } else {
+        return json_encode(array('success' => true));
+    }
+});
